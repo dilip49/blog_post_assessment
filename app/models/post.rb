120cb19bbfcd0fcdd16d48bfb324c987632153c8
Dilip Bhaidiya <dilip.bhaidiya@gmail.com>
2024-11-11 17:31:48 +0530

    Add logic to pass all  previous test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,18 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :file
+
+  enum status: { pending: 'pending', approved: 'approved', rejected: 'rejected' }
+
+  validates :title, :content, presence: true
+
+  after_initialize :set_default_status, if: :new_record?
+  
+  private
+
+  def set_default_status
+    self.status ||= 'pending'
+  end
 end
