d76b51f8426ecd9445b066623a42878a37f5a8a7
Dilip Bhaidiya <dilip.bhaidiya@gmail.com>
2024-11-11 17:30:17 +0530

    Remove file spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { should belong_to(:user) }
-  it { should have_one_attached(:file) }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:content) }
