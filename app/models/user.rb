426a43a553b1ff6be8bcf051c44f92465ec670e1
Dilip Bhaidiya <dilip.bhaidiya@gmail.com>
2024-11-13 12:31:58 +0530

    Add associations for the posts to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :posts
+
   def admin?
     role == 'admin'
   end
